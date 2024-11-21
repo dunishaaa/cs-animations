9233e5ba2f609dfe301faa0f0efafaeaecabd9c5
dunishaaa <aphimontes@outlook.com>
2024-11-21 13:57:23 -0600

    eratostenes sieve

--- erathostenes-sieve/main_scene.py
+++ erathostenes-sieve/main_scene.py
@@ -14,9 +14,14 @@ def normalPrime(n: int):
 
 
 def sievePrime(n: int):
-    l = [True for i in range(n + 1)]
-    l[1] = False
-    return l
+    is_prime = [True for i in range(n + 1)]
+    is_prime[0] = False
+    is_prime[1] = False
+    for i in range(2, n + 1):
+        if is_prime[i]:
+            for j in range(i + i, n + 1, i):
+                is_prime[j] = False
+    return is_prime
 
 
 class Sieve(Scene):
@@ -42,10 +47,43 @@ class Sieve(Scene):
         grid.scale(0.6)
         self.play(Create(grid), run_time=2)
 
-        primes = normalPrime(HEIGHT * WIDTH)
+        # primes = normalPrime(HEIGHT * WIDTH)
+        primes = sievePrime(HEIGHT * WIDTH)
 
-        for i, sq in enumerate(gm_sq_grid):
-            if primes[i + 1]:
-                sq.set_color(GREEN).set_fill(GREEN)
+        n: int = HEIGHT * WIDTH
+
+        is_prime = [True for _ in range(n + 1)]
+        is_prime[0] = False
+        is_prime[1] = False
+        rt = 0.7
+        curr_counter = 4
+        for i in range(2, n + 1):
+            if is_prime[i]:
+                if curr_counter > 0:
+                    self.play(gm_sq_grid[i - 1].animate.set_color(YELLOW), run_time=rt)
+                else:
+                    self.play(gm_sq_grid[i - 1].animate.set_color(YELLOW), run_time=0.3)
+                counter = 5
+                for j in range(i * i, n + 1, i):
+                    if is_prime[j]:
+                        is_prime[j] = False
+                        if counter >= 0:
+                            self.play(
+                                Succession(
+                                    Indicate(gm_sq_grid[j - 1], color=WHITE),
+                                    gm_sq_grid[j - 1].animate.set_color(RED),
+                                ),
+                                run_time=1,
+                            )
+                            counter -= 1
+                        else:
+                            self.play(
+                                Succession(
+                                    Indicate(gm_sq_grid[j - 1], color=WHITE),
+                                    gm_sq_grid[j - 1].animate.set_color(RED),
+                                ),
+                                run_time=0.1,
+                            )
+                self.play(gm_sq_grid[i - 1].animate.set_color(GREEN), run_time=rt)
 
         self.wait()
